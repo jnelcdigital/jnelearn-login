07bf5f7c473e4fec3edff9c43b53744907975bc6
jnelcdigital <jnelcdigital@gmail.com>
2024-08-12 16:50:38 +0700

    add ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+const PORT = process.env['PORT'] ?? '8080';
+
+module.exports = {
+  apps: [
+    {
+      name: "jne-login",
+      script: "npm",
+      args: "start",
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "dev",
+        PORT: PORT,
+      },
+    },
+  ],
+};
